6cb89ee78947ec06b8b7850f4ec00a457fb01f2f
Christian Yemele <yemelechristian2@gmail.com>
2026-05-16 16:33:01 +0100

    fix: add supervisor and global queue to NioScheduler to prevent deadlocks

--- core/jvm-native/src/main/scala/zio/internal/Blocking.scala
+++ core/jvm-native/src/main/scala/zio/internal/Blocking.scala
@@ -45,10 +45,12 @@ object Blocking {
 
   /**
    * Signals to the scheduler that the current thread is about to block. This
-   * method is a no-op except when the current thread is a ZScheduler.Worker. In
+   * method is a no-op except when the current thread is a scheduler worker. In
    * that case, the worker is marked as "blocking" and a new worker is spawned
    * to replace it.
    */
-  private[zio] final def signalBlocking(): Unit =
+  private[zio] final def signalBlocking(): Unit = {
     ZScheduler.markCurrentWorkerAsBlocking()
+    NioScheduler.markCurrentWorkerAsBlocking()
+  }
 }
